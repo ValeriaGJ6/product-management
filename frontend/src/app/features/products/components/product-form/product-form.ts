a48f6a9133e13944eb2bda2b5f103c54506cbd9d
Valeria Guerrero <valeriaaguerreroj@gmail.com>
2025-09-09 00:58:48 -0500

    feat: setup Angular Material and product components structure

---
+++ frontend/src/app/features/products/components/product-form/product-form.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-form.html',
+  styleUrls: ['./product-form.scss']
+})
+export class ProductForm {
+
+}
